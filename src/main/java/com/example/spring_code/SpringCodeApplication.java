4f43c676b534fa06eadd4fb0e1ff8269dbe5ed8f
saul <dlathf3210@naver.com>
2024-03-22 22:02:03 +0900

    [START]

---
+++ src/main/java/com/example/spring_code/SpringCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_code/SpringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
