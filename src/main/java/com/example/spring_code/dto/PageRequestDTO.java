075a7654ab304acf6fb87c5cee7a67351e9eafc4
saul <dlathf3210@naver.com>
2024-09-07 13:54:58 +0900

    add PageResponseDTO

--- src/main/java/com/example/spring_code/dto/PageRequestDTO.java
+++ src/main/java/com/example/spring_code/dto/PageRequestDTO.java
@@ -1,9 +1,15 @@
 package com.example.spring_code.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @SuperBuilder
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class PageRequestDTO {
 
     @Builder.Default
--- src/main/java/com/example/spring_code/dto/PageResponseDTO.java
+++ src/main/java/com/example/spring_code/dto/PageResponseDTO.java
@@ -1,5 +1,52 @@
 package com.example.spring_code.dto;
 
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Data
 public class PageResponseDTO<E> {
+
+    private List<E> listDto;
+
+    private List<Integer> pageNumList;
+
+    private PageRequestDTO pageRequestDTO;
+
+    private boolean prev, next;
+
+    private int totalPage, totalCount, prevPage, nextPage, current;
+
+
+    PageResponseDTO(List<E> listDto, PageRequestDTO pageRequestDTO, long total){
+        this.listDto = listDto;
+        this.pageRequestDTO = pageRequestDTO;
+        this.totalCount = (int) total;
+
+        // 끝 페이지 계산
+        int endPageNum = (int) (10 * (Math.ceil(pageRequestDTO.getPage() / 10.0)));
+        
+        // 시작 페이지 계산
+        int startPageNum = endPageNum - 9;
+
+        // 최종 마지막 페이지 계산
+        int lastPageNum = (int) Math.ceil(total / (double)pageRequestDTO.getSize());
+
+
+        endPageNum = endPageNum > lastPageNum ? lastPageNum : endPageNum;
+
+        this.prev = startPageNum > 1;
+        this.next = total > (long) endPageNum * pageRequestDTO.getSize();
+        this.pageNumList = IntStream.range(startPageNum, endPageNum).boxed().collect(Collectors.toList());
+        this.prevPage = prev ? startPageNum - 1 : 0;
+        this.nextPage = next ? endPageNum + 1 : 0;
+
+
+
+    }
+
+
 
 }
