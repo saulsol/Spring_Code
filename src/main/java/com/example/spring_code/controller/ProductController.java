bb5783d452ee681e9ee9776d0a835579f8a7eeae
saul <dlathf3210@naver.com>
2024-10-27 22:44:14 +0900

    add delete file service

--- src/main/java/com/example/spring_code/controller/ProductController.java
+++ src/main/java/com/example/spring_code/controller/ProductController.java
@@ -4,13 +4,12 @@ import com.example.spring_code.dto.ProductDTO;
 import com.example.spring_code.util.CustomFileUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
@@ -32,6 +31,11 @@ public class ProductController {
 
         log.info(uploadFileNames);
         return ResponseEntity.ok().body(Map.of("SUCCESS", "RESULT"));
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFile(@PathVariable("fileName") String fileName) {
+        return customFileUtil.getFile(fileName);
     }
 
 
